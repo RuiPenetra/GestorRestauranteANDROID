4930b106f4f2cf5a32fe5bfd732ad36e176245a8
carlos <2180687@my.ipleiria.pt>
2020-10-21 16:40:29 +0100

    Criação de Layout Main , Registo , Login , Contactos

---
+++ ProjetoAndroid/app/src/main/java/com/example/myapplication/ContactosActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Contactos");
+        setContentView(R.layout.activity_contactos);
+    }
+}
---
+++ ProjetoAndroid/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Register");
+        setContentView(R.layout.activity_register);
+    }
+}
